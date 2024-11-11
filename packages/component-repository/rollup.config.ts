6678ce6120f2c3888bea51cb7218245878cae5d2
ZARD0520 <422451702@qq.com>
2024-11-11 22:25:28 +0800

    feat：继续完善项目初始化结构

--- packages/component-repository/rollup.config.ts
+++ packages/component-repository/rollup.config.ts
@@ -1,14 +1,24 @@
 import babel from '@rollup/plugin-babel';
-import { terser } from '@rollup/plugin-terser';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript'
 
 const config = {
-    input: 'src/index.js',
+    input: {
+        'react': './react/index.ts',
+        'vue': './vue/index.ts'
+    },
     output: {
-        name: 'core',
-        file: 'dist/index.js',
-        format: 'umd',
+        'react': {
+            file: 'dist/react/bundle.js',
+            format: 'umd'
+        },
+        'vue': {
+            file: 'dist/vue/bundle.js',
+            format: 'umd'
+        }
     },
     plugins: [
+        typescript(),
         babel({
             exclude: 'node_modules/**', // 排除 node_modules
             babelHelpers: 'bundled',
