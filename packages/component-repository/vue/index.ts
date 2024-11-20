418654b2dd1a3c1f807c06b38bf18c58633a59a0
ZARD0520 <422451702@qq.com>
2024-11-20 23:43:39 +0800

    update：vue component-lib

--- packages/component-repository/vue/index.ts
+++ packages/component-repository/vue/index.ts
@@ -1,14 +1,58 @@
 import { App, DefineComponent } from 'vue'
-import Antd from 'ant-design-vue'
+// 导入基础组件
+import {
+  Button,
+  Input,
+  InputNumber,
+  Select,
+  SelectOption,
+  Radio,
+  RadioGroup,
+  Checkbox,
+  CheckboxGroup,
+  DatePicker,
+  Switch
+} from 'ant-design-vue'
+// 导入类型
+import type { ButtonProps } from 'ant-design-vue/es/button'
+import type { InputProps } from 'ant-design-vue/es/input'
+import type { InputNumberProps } from 'ant-design-vue/es/input-number'
+import type { SelectProps, OptionProps } from 'ant-design-vue/es/select'
+import type { RadioProps, RadioGroupProps } from 'ant-design-vue/es/radio'
+import type { CheckboxProps, CheckboxGroupProps } from 'ant-design-vue/es/checkbox'
+import type { DatePickerProps } from 'ant-design-vue/es/date-picker'
+import type { SwitchProps } from 'ant-design-vue/es/switch'
+// 导入样式
 import 'ant-design-vue/dist/antd.css'
-// 引入二次封装组件
+// 导入二次封装组件
 
 interface ComponentObject {
-  [key: string]: DefineComponent<{}, {}, any>;
+  'z-button'?: ButtonProps
+  'z-input'?: InputProps
+  'z-input-number'?: InputNumberProps
+  'z-select'?: SelectProps
+  'z-select-option'?: OptionProps
+  'z-radio'?: RadioProps
+  'z-radio-group'?: RadioGroupProps
+  'z-checkbox'?: CheckboxProps
+  'z-checbbox-group'?: CheckboxGroupProps
+  'z-date-picker'?: DatePickerProps
+  'z-switch'?: SwitchProps
 }
 
 const components: ComponentObject = {
   // 存放组件
+  'z-button': Button as ButtonProps,
+  'z-input': Input as InputProps,
+  'z-input-number': InputNumber as InputNumberProps,
+  'z-select': Select as SelectProps,
+  'z-select-option': SelectOption as OptionProps,
+  'z-radio': Radio as RadioProps,
+  'z-radio-group': RadioGroup as RadioGroupProps,
+  'z-checkbox': Checkbox as CheckboxProps,
+  'z-checbbox-group': CheckboxGroup as CheckboxGroupProps,
+  'z-date-picker': DatePicker as DatePickerProps,
+  'z-switch': Switch as SwitchProps
 }
 
 export default {
