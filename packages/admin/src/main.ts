e8c373323da4bc5e6a265771b5eb135d8f08bf53
ZARD0520 <422451702@qq.com>
2025-01-09 22:57:22 +0800

    update：组件库不再处理element，仅需其他引入项目保持版本即可

--- packages/admin/src/main.ts
+++ packages/admin/src/main.ts
@@ -2,6 +2,13 @@ import { createApp } from 'vue'
 import '@/style.css'
 import App from '@/App.vue'
 import ComponentLib from 'component-repo-vue'
+import {
+  ElButton
+} from 'element-plus'
+import router from '@/router/index'
 
 const app = createApp(App)
+
+app.use(router)
+app.use(ElButton)
 app.use(ComponentLib).mount('#app')
--- packages/admin/src/views/home/index.vue
+++ packages/admin/src/views/home/index.vue
@@ -1,1 +1,11 @@
-<template></template>
+<template>
+  <el-button>6</el-button>
+  <z-table>6</z-table>
+</template>
+
+<script setup>
+import { getCurrentInstance } from 'vue';
+
+// const p = getCurrentInstance()
+// console.log(p)
+</script>
